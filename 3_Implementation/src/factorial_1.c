c237b752fef7759052c3e9325c2b61f9184ea4c7
99003746 <nitin.shetty1@ltts.com>
2021-02-15 04:58:32 +0530

    updated if else statement in product fun()

--- 3_Implementation/src/factorial_1.c
+++ 3_Implementation/src/factorial_1.c
@@ -1,4 +1,30 @@
 #include <stdio.h> 
 #include<math.h>
 
+void product(int a, int b)
+{
+    if(a==0)
+    {
+        printf("ERROR");
+    }
+    else if(b==0)
+    {
+        printf("ERROR");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
+{
+
+}
