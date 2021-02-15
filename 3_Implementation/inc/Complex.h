283495a7c627b53627bfc8953598378246fa8790
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 10:34:58 +0530

    variable declared as int.

--- 3_Implementation/inc/Complex.h
+++ 3_Implementation/inc/Complex.h
@@ -2,10 +2,10 @@
 #define __COMPLEX_H__
 
 float re1,im1,re2,im2;
-
+int Com_Option;
 
 void input(void);
-int  option(int a);
+int  option(void);
 float Com_add(float re1, float re2, float im1, float im2);
 
 #endif 
