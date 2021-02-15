cbaf19d24ef32b55d99a9e7fabe5e30b38bb549a
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 10:33:47 +0530

    Scanf function added.

--- 3_Implementation/src/Complex.c
+++ 3_Implementation/src/Complex.c
@@ -5,7 +5,7 @@
 void input(void)
 {
     printf("Enter the complex number with format re1 im1 re2 im2");
-    Scanf(" f% f% f% f% ", &re1,&im1, &re2, &im2);
+    scanf(" f% f% f% f% ", &re1,&im1, &re2, &im2);
     printf(" Choose your option :");
     option();
 }
@@ -16,6 +16,7 @@ int option(int a)
      printf("\nAvailable Operations\n");
     printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
     printf("\n\tEnter your choice\n");
+    scanf("%d", &Com_Option);
 
 
 }
