523ffa552ffdf77549fb123ab7e0e6447243bc3d
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 10:43:45 +0530

    Com_add function completed with code.

--- 3_Implementation/src/Complex.c
+++ 3_Implementation/src/Complex.c
@@ -22,12 +22,16 @@ int option(int a)
         Com_add(re1,im1,re2,im2);
     }
 
-
+return 0;
 }
 
-float Com_add(float re1, float re2, float im1, float im2)
+float Com_add(float re1, float im1, float re2, float im2)
 {
-          float res=0;
+          float res1=0, res2 = 0;
+          res1 = (re1+re2);
+          res2 = (im1+im2);
+          printf(" The addition of given complex number is : %f i%f", res1,re2);
+          return 0;
 
 
 
