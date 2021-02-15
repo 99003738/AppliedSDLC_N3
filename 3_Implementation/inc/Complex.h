cb14fa2e68cac20c306c3968d3382f349e3d862f
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 11:03:22 +0530

    created function com_sub,cim_mul.

--- 3_Implementation/inc/Complex.h
+++ 3_Implementation/inc/Complex.h
@@ -7,5 +7,8 @@ int Com_Option;
 void input(void);
 int  option(void);
 float Com_add(float re1, float re2, float im1, float im2);
+float Com_sub(float re1, float re2, float im1, float im2);
+float Com_mul(float re1, float re2, float im1, float im2);
+
 
 #endif 
