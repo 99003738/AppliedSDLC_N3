a3ac3eb38e970ee1535b64ff9c79bbdc8a9817eb
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 10:32:41 +0530

    writing code in option folder.

--- 3_Implementation/src/Complex.c
+++ 3_Implementation/src/Complex.c
@@ -6,13 +6,18 @@ void input(void)
 {
     printf("Enter the complex number with format re1 im1 re2 im2");
     Scanf(" f% f% f% f% ", &re1,&im1, &re2, &im2);
-    printf(" Cjoice your option :");
+    printf(" Choose your option :");
     option();
 }
 
 int option(int a)
 {
  
+     printf("\nAvailable Operations\n");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
+    printf("\n\tEnter your choice\n");
+
+
 }
 
 float Com_add(float re1, float re2, float im1, float im2)
