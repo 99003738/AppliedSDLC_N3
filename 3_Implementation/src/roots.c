9a863c5af747cf655646e63c6c7fba6597994020
99003743 <78868680+99003743@users.noreply.github.com>
2021-02-15 07:13:03 +0530

    code for program

--- 3_Implementation/src/roots.c
+++ 3_Implementation/src/roots.c
@@ -2,3 +2,12 @@
 #include <math.h>
 
 int main()
+{
+double num, root;
+
+    /* Input a number from user */
+    printf("Enter any number to find square root: ");
+    scanf("%lf", &num);
+
+    /* Calculate square root of num */
+    root = sqrt(num);
