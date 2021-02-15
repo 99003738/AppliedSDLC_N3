22cf6ea1aa8a9f40c0f22d55e85d87ac4064d2f2
99003738 <abhishek.mishra1@ltts.com>
2021-02-15 10:28:00 +0530

    writin gcode for taking the input from user.

--- 3_Implementation/src/Complex.c
+++ 3_Implementation/src/Complex.c
@@ -4,12 +4,15 @@
 
 void input(void)
 {
-
+    printf("Enter the complex number with format re1 im1 re2 im2");
+    Scanf(" f% f% f% f% ", &re1,&im1, &re2, &im2);
+    printf(" Cjoice your option :");
+    option();
 }
 
 int option(int a)
 {
-
+ 
 }
 
 float Com_add(float re1, float re2, float im1, float im2)
