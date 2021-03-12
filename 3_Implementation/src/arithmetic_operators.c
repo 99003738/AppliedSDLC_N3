2258b190ee844fe9d9bc06a32be374ff012a3429
99003737 <jeshwanth.ega@ltts.com>
2021-03-12 09:45:12 +0530

    changed

--- 3_Implementation/src/arithmetic_operators.c
+++ 3_Implementation/src/arithmetic_operators.c
@@ -51,9 +51,9 @@ int main()
 #include"arithmetic_operators.h"
 
 int addition (int operand1,int operand2);
-int subtraction (int operand 1,int operand2);
+int subtraction (int operand1,int operand2);
 int multiplication (int operand1, int operand2);
-int division (int operand 1, int operand2);
+int division (int operand1, int operand2);
 
          /*ADDITION*/
 int addition (int operand1,int operand2)
@@ -63,7 +63,29 @@ int addition (int operand1,int operand2)
     return sum;
 }
 
-int
+int subtraction (int operand1,int operand2);
+{
+    int sub;
+    sub = operand1-operand2;
+    return sub;
+}
+int multiplication (int operand1, int operand2);
+{
+    int mul;
+    mul = operand1*operand2;
+    return mul;
+}
+int division (int operand1, int operand2);
+    
+{
+    int div;
+    div = operand1+operand2;
+    return div;
+}
+
+
+
+
 
 
 
