a99cf08010953d7abc2060014d3640a1776f15d9
99003737 <jeshwanth.ega@ltts.com>
2021-03-12 09:48:27 +0530

    updated header files

--- 3_Implementation/src/arithmetic_operators.c
+++ 3_Implementation/src/arithmetic_operators.c
@@ -63,19 +63,19 @@ int addition (int operand1,int operand2)
     return sum;
 }
 
-int subtraction (int operand1,int operand2);
+int subtraction (int operand1,int operand2)
 {
     int sub;
     sub = operand1-operand2;
     return sub;
 }
-int multiplication (int operand1, int operand2);
+int multiplication (int operand1, int operand2)
 {
     int mul;
     mul = operand1*operand2;
     return mul;
 }
-int division (int operand1, int operand2);
+int division (int operand1, int operand2)
     
 {
     int div;
