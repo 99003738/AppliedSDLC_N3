49d9133f46ccae71a0f6f3098c79695db98e5326
99003737 <78848865+99003737@users.noreply.github.com>
2021-03-13 15:25:48 +0530

    Update arithmetic_operators.c

--- 3_Implementation/src/arithmetic_operators.c
+++ 3_Implementation/src/arithmetic_operators.c
@@ -1,59 +1,10 @@
-/*#include<stdio.h>
-#include <calculator_operations.h>
-int main()
-{
-    float operand1, operand2,a,b,c,d,e;
-     
-    char operators;
-    printf("ENTER THE OPERATOR (+ , - , * , / ) :  ");
-    scanf("%c",&operators);
-
-    printf("ENTER THE TWO OPERANDS :  ");
-    
-    scanf("%f %f",&operand1,&operand2);
-
-    switch(operators){
-        
-        case '+':
-        a = operand1+operand2;
-        printf(" %f + %f= %.2f",operand1,operand2,a);
-        
-        break;
-        
-        case '-':
-        b = operand1-operand2;
-        printf(" %f - %f= %.2f",operand1,operand2,b);
-        
-        break;
-        
-        case '*':
-        c = operand1*operand2; 
-        printf(" %f * %f= %.2f",operand1,operand2,c);
-       
-        break;
-        
-        case '/':
-        d = operand1/operand2;
-        printf(" %f / %f= %.2f",operand1,operand2,d);
-       
-        break;
-        
-        default:
-        printf("operator is not correct");
-         }
-    
-       
-        return 0;
-    
-}*/
+#include <f.h>
 
 
-#include"arithmetic_operators.h"
-
-int addition (int operand1,int operand2);
+/*int addition (int operand1,int operand2);
 int subtraction (int operand1,int operand2);
 int multiplication (int operand1, int operand2);
-int division (int operand1, int operand2);
+int division (int operand1, int operand2);*/
 
          /*ADDITION*/
 int addition (int operand1,int operand2)
@@ -79,15 +30,6 @@ int division (int operand1, int operand2)
     
 {
     int div;
-    div = operand1+operand2;
+    div = operand1/operand2;
     return div;
 }
-
-
-
-
-
-
-
-
-  
