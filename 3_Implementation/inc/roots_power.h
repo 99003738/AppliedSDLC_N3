4b5f3c2a0b887ee7aa2f7ef5f77898510a452ef8
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-13 16:44:54 +0530

    Create roots_power.h

---
+++ 3_Implementation/inc/roots_power.h
@@ -0,0 +1,13 @@
+#ifndef _ROOTS_POWER_H_
+
+#define _ROOTS_POWER_H_
+#include<stdio.h>
+
+
+#include <math.h>
+
+void roots();
+double cuberoot(double); //function for cuberoot
+double squareroot(double); //function for squareroot
+double exponential(double);
+#endif
