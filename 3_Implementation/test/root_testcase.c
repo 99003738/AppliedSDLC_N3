7edfd084b82fbd5f842b8bbe21bb6e08f874b885
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-13 16:53:48 +0530

    Create root_testcase.c

---
+++ 3_Implementation/test/root_testcase.c
@@ -0,0 +1,54 @@
+#include "unity.h"
+#include "roots_power.h"
+
+
+/* Modify these two lines according to the project */
+#define PROJECT_NAME    "Calculator"
+
+/* Prototypes for all the test functions */
+
+void test_squareroot(void);
+void test_cuberoot(void);
+void test_exponential(void);
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  
+  RUN_TEST(test_squareroot);
+  RUN_TEST(test_cuberoot);
+  RUN_TEST(test_exponential);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+
+void test_squareroot(void) {
+  TEST_ASSERT_EQUAL(12, squareroot(144));
+  
+  /* Dummy fail*/
+  //TEST_ASSERT_EQUAL(10, squareroot(400));
+}
+void test_cuberoot(void) {
+  TEST_ASSERT_EQUAL(2, cuberoot(8));
+  
+  /* Dummy fail*/
+  //TEST_ASSERT_EQUAL(10, squareroot(400));
+}
+void test_exponential(void) {
+  TEST_ASSERT_EQUAL(2.7, exponential(1));
+  
+  /* Dummy fail*/
+  //TEST_ASSERT_EQUAL(10, squareroot(400));
+}
