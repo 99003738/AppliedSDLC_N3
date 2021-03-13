59a12ce9142c803becc7a62ed80aad2822bd7cce
99003746 <78853902+99003746@users.noreply.github.com>
2021-03-13 14:59:42 +0530

    Create fact_log.c

---
+++ 3_Implementation/src/fact_log.c
@@ -0,0 +1,84 @@
+#include"fact_log.h"
+
+//Logarithm Product Function
+
+float product(float a, float b)
+{
+    if(a==0)
+    {
+      return 0;
+    }
+    else if(b==0)
+    {
+      return 0;
+    }
+    else if(a<0)
+    {
+      return 0;
+    }
+    else if(b<0)
+    {
+      return 0;
+    }
+    else
+    {
+      double result;
+      result=log10(a)+log10(b);
+      return result;
+    }
+}
+
+                //Logarithm Ratios Function
+float ratios(float a, float b) 
+{
+    if(a==0)
+    {
+      
+       return 0;
+    }
+    else if(b==0)
+    {
+        return 0;
+    }
+    else if(a<0)
+    {
+        return 0;
+    }
+    else if(b<0)
+    {
+        return 0;
+    }
+    else
+    {
+        double result;
+        result=log10(a)-log10(b);
+        return result;
+    }
+}
+              //Factorial Function
+
+int factorial(int n)
+{
+    int i,fact=1;
+    
+    if(n==1)
+    {
+        return 1;
+    }
+    else if(n==0)
+    {
+        return 1;
+    }
+    else if(n<0)
+    {
+        return 0;
+    }
+    else
+    {
+        for(i=1; i<=n; i++)
+    {
+        fact=fact*i;
+    }
+        return fact;
+    }
+}
